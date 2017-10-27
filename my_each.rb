3053200d009dbdf21588fa27496dee45a5c8fbb7
Nolan Hughes <nhughes987@gmail.com>
2017-10-27 17:28:03 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,19 @@
 def my_each(array)
-  # while
-
-  # end
-
-  yield
+  new_ary = []
+  self.each do |elem|
+    new_ary << yield(elem)
+  end
+  new_ary
 end
 
 # my_each(array) do |i|
 
 # end
+
+def kollect
+  new_ary = []
+  self.each do |elem|
+    new_ary << yield(elem)
+  end
+  new_ary
+end
